6ec5df988eee8438775a1b5fff9bec3809850002
you-bowen <2756456886@qq.com>
2023-10-10 22:19:27 +0800

    add some commands

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,35 @@
 import * as vscode from 'vscode';
 import { copy } from "./clipboard";
 
+import * as path from 'path';
+
+function copyRelativePath(selected: any) {
+    const editor = vscode.window.activeTextEditor;
+
+    let current;
+    let relativePath;
+
+    if (!editor) {
+        vscode.window.showInformationMessage("There is no open file to get the relative path.");
+        return;
+    }
+
+    current = editor.document.uri;
+    relativePath = path.relative(path.dirname(current.path), selected.path);
+    relativePath = relativePath.replace(/\\/g, '/');
+
+    // if (config.addLeadingDot && !relativePath.startsWith('..')) {
+    //     relativePath = `./${relativePath}`;
+    // }
+
+    copy(relativePath);
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
+	const vs_console = vscode.window.createOutputChannel('vsc-refer-link');
+	vs_console.appendLine("init");
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "code-refer-ts" is now active!');
@@ -34,13 +58,32 @@ export function activate(context: vscode.ExtensionContext) {
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
 			editor.edit(editBuilder => {
-				editor.selections.forEach(selection => {
-					editBuilder.insert(selection.active, 'aaa');
+				editor.selections.forEach((selection, idx) => {
+					editBuilder.insert(selection.active, String.fromCharCode(idx+97));
 				});
 			});
 		}
 	});
-
+	context.subscriptions.push(disposable1);
+	let disposable2 = vscode.commands.registerCommand("code-refer-ts.RepeatLastCommand", ()=>{
+		// currently unavailable
+		vs_console.appendLine("before patch");
+		// function xx<T = unknown>(command: string, ...rest: any[]): Thenable<T> {
+		// 	// 在这里执行您的命令逻辑
+		// 	// 返回一个 Thenable 对象，例如使用 vscode.commands.executeCommand() 函数
+		// 	vs_console.appendLine(command);
+		// 	return vscode.commands.executeCommand<T>(command, ...rest);
+		// }
+		// vscode.commands.executeCommand = xx;
+	});
+	context.subscriptions.push(disposable2);
+	let disposable3 = vscode.commands.registerCommand("code-refer-ts.CopyWindowRelativePath", (uri) => {
+		// vscode.commands.executeCommand('editor.action.copyRelativePath');
+		// uri will be like file:///Users/xxx/xxx.vue
+		vs_console.appendLine(uri);
+		copyRelativePath(uri);
+	});
+	context.subscriptions.push(disposable3);
 }
 
 // This method is called when your extension is deactivated
